6376095d69def2ea49e3117099ee48664b31acce
Niels Mündler <n.muendler@posteo.de>
2023-01-05 14:47:52 +0100

    Add pre-commit

--- pluthon/__init__.py
+++ pluthon/__init__.py
@@ -9,7 +9,7 @@ try:
 except ImportError as e:
     warnings.warn(ImportWarning(e))
 
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 1)
 
 __version__ = ".".join([str(i) for i in VERSION])
 __author__ = "nielstron"
