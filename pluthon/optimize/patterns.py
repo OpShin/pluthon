b76b4abba5fd194f174a940c79dde1a90895e8a2
Niels Mündler <n.muendler@posteo.de>
2023-09-18 13:54:34 +0200

    Fixes

--- pluthon/optimize/patterns.py
+++ pluthon/optimize/patterns.py
@@ -9,16 +9,26 @@ from ..util import NodeTransformer, NodeVisitor, iter_fields
 class PatternCollector(NodeVisitor):
     def __init__(self):
         self.pattern_classes = set()
+        self.pattern_classes_in_dep_order = list()
 
     def visit(self, node):
         """Visit a node."""
+        added_patterns = []
         while isinstance(node, Pattern):
             # Patterns are special
-            self.pattern_classes.add(type(node))
+            # we collect them here and later add them in reverse order
+            # after subpatterns are added recursively
+            # this ensures that the outermost pattern is added last
+            node_type = type(node)
+            if node_type not in self.pattern_classes:
+                added_patterns.append(node_type)
+            self.pattern_classes.add(node_type)
             node = node.compose()
         method = "visit_" + node.__class__.__name__
         visitor = getattr(self, method, self.generic_visit)
-        return visitor(node)
+        res = visitor(node)
+        self.pattern_classes_in_dep_order.extend(reversed(added_patterns))
+        return res
 
 
 def make_abstract_function(pattern_class: Type[Pattern]):
@@ -50,7 +60,7 @@ class PatternReplacer(NodeTransformer):
     def visit_Program(self, node: Program):
         pattern_collector = PatternCollector()
         pattern_collector.visit(node)
-        pattern_classes = pattern_collector.pattern_classes
+        pattern_classes = pattern_collector.pattern_classes_in_dep_order
         # TODO should we not somehow figure out interdependencies here
         term = PLet(
             [
