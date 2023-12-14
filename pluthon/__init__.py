e6f07c8772e6578820341b3222d62ccb81811dd0
Niels Mündler <n.muendler@posteo.de>
2023-12-14 23:55:09 +0100

    New patch version 0.4.3

--- pluthon/__init__.py
+++ pluthon/__init__.py
@@ -14,11 +14,11 @@ except ImportError as e:
         exc_info=e,
     )
 
-VERSION = (0, 4, 2)
+VERSION = (0, 4, 3)
 
 __version__ = ".".join([str(i) for i in VERSION])
 __author__ = "nielstron"
 __author_email__ = "n.muendler@web.de"
 __copyright__ = "Copyright (C) 2023 nielstron"
 __license__ = "MIT"
-__url__ = "https://github.com/imperatorlang/pluthon"
+__url__ = "https://github.com/opshin/pluthon"
